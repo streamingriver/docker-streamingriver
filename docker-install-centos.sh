1a8ec7e1418159c50ba7007835fdb16c0f14798b
Mindaugas Žvirblis <mz@9g.lt>
2021-11-23 10:34:12 +0200

    docker install scripts for different operating systems

---
+++ docker-install-centos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum-config-manager --enable docker-ce-nightly
+
+sudo yum install docker-ce docker-ce-cli containerd.io
---
+++ docker-install-fedora.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+sudo dnf -y install dnf-plugins-core
+
+
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf config-manager --set-enabled docker-ce-nightly
+
+sudo dnf install docker-ce docker-ce-cli containerd.io
