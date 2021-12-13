7dec95456c40dde86ee5b02a1f864864859ee10c
Mindaugas Žvirblis <mz@9g.lt>
2021-12-13 08:45:29 +0200

    chmod docker compose and nicer install.sh

--- docker-install-centos.sh
+++ docker-install-centos.sh
@@ -21,7 +21,7 @@ yum -y install docker-ce docker-ce-cli containerd.io
 
 install_docker_compose () {
 	curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
-	shasum -a 256 -s -c docker-compose-linux-x86_64.sha256 && mv ./docker-compose-linux-x86_64 /usr/local/sbin/docker-compose && rm -f docker-compose-linux-x86_64.sha256 && return 0
+	shasum -a 256 -s -c docker-compose-linux-x86_64.sha256 && mv ./docker-compose-linux-x86_64 /usr/local/sbin/docker-compose && chmod +x /usr/local/sbin/docker-compose && rm -f docker-compose-linux-x86_64.sha256 && return 0
 	echo "problem installing docker compose"
 	exit 666
 }
